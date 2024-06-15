6b42a66b8f8714602198ae95fb19b3da2172c6cd
RayenRk <rakkadrayen@gmail.com>
2024-06-15 23:08:10 +0100

    🔑 Auth with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://charming-herring-31.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import LeftPanel from "@/components/home/left-panel";
 import RightPanel from "@/components/home/right-panel";
 
 export default function Home() {
-
-	return (
-		<main className='m-5'>
-	<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
-		<LeftPanel />
-		<RightPanel />
-	</div>
-</main>
-	);
+  return (
+    <main className="m-5">
+      <div className="flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
+        <LeftPanel />
+        <RightPanel />
+      </div>
+    </main>
+  );
 }
