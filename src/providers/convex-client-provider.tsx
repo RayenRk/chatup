31d240f27612d950184a561b25e2a1a0a25b33ce
RayenRk <rakkadrayen@gmail.com>
2024-06-16 02:11:19 +0100

    🟨 Get group members

--- src/providers/convex-client-provider.tsx
+++ src/providers/convex-client-provider.tsx
@@ -4,12 +4,15 @@ import { ReactNode } from "react";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { dark } from "@clerk/themes";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
 	return (
-		<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+		<ClerkProvider appearance={{
+			baseTheme: dark,
+		}} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
 				{children}
 			</ConvexProviderWithClerk>
